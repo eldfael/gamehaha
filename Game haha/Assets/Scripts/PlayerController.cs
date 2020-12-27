243db6870dddb680af30fab9c77f41cdddab6b44
Eldfael <eddie.rayner1@gmail.com>
2020-12-27 14:37:51 +0000

    Player Added

---
+++ Game haha/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float FIXEDPLAYERSPEED = 5.0f;
+
+    Vector2 inputDirection;
+    Rigidbody2D playerRigidbody;
+
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        inputDirection = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) ;
+    }
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = inputDirection.normalized * FIXEDPLAYERSPEED;
+    }
+}
