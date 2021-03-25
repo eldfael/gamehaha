61d471a4067f3701f20abc4ecb6f782dd5df1319
Eldfael <eddie.rayner1@gmail.com>
2021-03-25 04:16:02 +0000

    Basic Projectiles Updated
    basic collision, cooldown added, other minor tweaks

--- Game haha/Assets/Scripts/ProjectileController.cs
+++ Game haha/Assets/Scripts/ProjectileController.cs
@@ -9,8 +9,13 @@ public class ProjectileController : MonoBehaviour
     bool created = false;
     Vector2 direction;
     float speed = 20f;
-    float maxduration = 0.25f;
-    float duration = 0;
+    float duration = 0.25f;
+    float durationTimer = 0;
+
+    List<GameObject> hits = new List<GameObject>();
+    ContactFilter2D filter;
+    List<Collider2D> collisions = new List<Collider2D>();
+
     void Start()
     {
         projectileRigidbody = GetComponent<Rigidbody2D>();
@@ -22,23 +27,45 @@ public class ProjectileController : MonoBehaviour
         if (created)
         {
             projectileRigidbody.velocity = direction * speed;
-            if (duration <= maxduration)
+            if (durationTimer <= duration)
             {
-                duration += Time.fixedDeltaTime;
+                durationTimer += Time.fixedDeltaTime;
             }
             else
             {
                 Destroy(gameObject);
             }
+
+            Physics2D.OverlapCollider(GetComponent<BoxCollider2D>(), filter, collisions);
+
+            if (collisions.Count > 0)
+            {
+
+                collisions.ForEach(delegate (Collider2D c)
+                {
+                    if (c.CompareTag("Enemy") && !hits.Contains(c.gameObject))
+                    {
+                        //ADD ENEMY HIT EVENT HERE
+                        hits.Add(c.gameObject);
+                    }
+                    if (c.CompareTag("Wall"))
+                    {
+                        Destroy(gameObject);
+                    }
+
+                });
+
+
+            }
         }
     }
+
 
     public void OnCreate(Vector2 _direction)
     {
         if(_direction != Vector2.zero) { direction = _direction; } else { direction = new Vector2(0, 1); }
 
         created = true;
-        Debug.Log("Created");
     }
 
 
--- Game haha/Assets/Scripts/Skills/AttackSkill1.cs
+++ Game haha/Assets/Scripts/Skills/AttackSkill1.cs
@@ -7,11 +7,14 @@ public class AttackSkill1 : MonoBehaviour, Skill
     PlayerController playerController;
     public GameObject projectile;
     Vector2 mousePos;
+    float cooldown;
+    float cooldownTimer;
 
     void Start()
     {
         playerController = transform.parent.GetComponent<PlayerController>();
-
+        cooldown = 0.4f;
+        cooldownTimer = 0.5f;
     }
 
     void Update()
@@ -19,16 +22,28 @@ public class AttackSkill1 : MonoBehaviour, Skill
         
     }
 
-    public void UseSkill() 
+    void FixedUpdate()
     {
-        mousePos = playerController.GetMousePosition();
-        GameObject newProjectile = Instantiate(projectile,
-            new Vector2(
-                playerController.transform.position.x + mousePos.normalized.x,
-                playerController.transform.position.y + mousePos.normalized.y), 
-                Quaternion.identity);
+        if (cooldownTimer <= cooldown)
+        {
+            cooldownTimer += Time.fixedDeltaTime;
+        }
+    }
 
-        newProjectile.GetComponent<ProjectileController>().OnCreate(mousePos.normalized);
-        newProjectile.GetComponent<Transform>().Rotate(0,0,Mathf.Atan2(mousePos.y,mousePos.x) * Mathf.Rad2Deg - 90f, Space.Self);
+    public void UseSkill()
+    {
+        if (cooldownTimer >= cooldown)
+        {
+            cooldownTimer = 0;
+            mousePos = playerController.GetMousePosition();
+            GameObject newProjectile = Instantiate(projectile,
+                new Vector2(
+                    playerController.transform.position.x + mousePos.normalized.x*0.5f,
+                    playerController.transform.position.y + mousePos.normalized.y*0.5f),
+                    Quaternion.identity);
+
+            newProjectile.GetComponent<ProjectileController>().OnCreate(mousePos.normalized);
+            newProjectile.GetComponent<Transform>().Rotate(0, 0, Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg - 90f, Space.Self);
+        }
     }
 }
